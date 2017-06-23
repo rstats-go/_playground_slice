7936217aacba7d9071c93d5ded24746aa987f779
Romain Francois <romain@thinkr.fr>
2017-06-23 11:26:53 +0200

    asserting the right type

--- src/go/src/main/main.c
+++ src/go/src/main/main.c
@@ -4,11 +4,13 @@
 #include "_cgo_export.h"
 
 SEXP sum_int( SEXP x ){
+  if( TYPEOF(x) != INTSXP ) error("expecting an integer vector") ;
   GoSlice gox = { INTEGER(x), LENGTH(x), LENGTH(x) } ;
   return ScalarInteger( SumInt(gox) ) ;
 }
 
 SEXP sum_double( SEXP x ){
+  if( TYPEOF(x) != REALSXP ) error("expecting a numeric vector") ;
   GoSlice gox = { REAL(x), LENGTH(x), LENGTH(x) } ;
   return ScalarReal( SumDouble(gox) ) ;
 }
